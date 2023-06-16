9f94ade05058a1b9e442fcb999fd43ec34361ad4
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-16 14:29:23 +0100

    Refactored code.

--- lib/playground.dart
+++ lib/playground.dart
@@ -1,21 +1,8 @@
-import 'dart:io';
-
 void main(List<String> args) {
   print(
     "================== Program to print DIVISORS OF A NUMBER =================",
   );
-  stdout.write(
-    "Enter a number: ",
+  print(
+    "Print new line \n",
   );
-  int userInput = int.parse(
-    stdin.readLineSync().toString(),
-  );
-
-  for (var i = 1; i <= userInput; i++) {
-    if (userInput % i == 0) {
-      print(
-        i,
-      );
-    }
-  }
 }
