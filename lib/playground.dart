cda39b8550f354b8f17d34a1d45398f32cc1d41b
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-17 11:37:55 +0100

    refactored code.

--- lib/playground.dart
+++ lib/playground.dart
@@ -1,22 +1,28 @@
 void main(List<String> args) {
-  List a = [
-    1,
-    1,
-    2,
-    3,
-    5,
-    8,
-    13,
-    21,
-    34,
-    55,
-    89,
-  ];
-  for (var i in a) {
-    if (i < 5) {
-      print(
-        i,
-      );
-    }
-  }
+  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  // for (var i in a) {
+  //   for (var j in b) {
+  //     if (i == j) {
+  //       c.add(i);
+  //     }
+  //   }
+  // }
+  // print(
+  //   c.toList(),
+  // );
+
+  // One liner using set intersections
+
+  print(
+    Set.from(
+      a,
+    ).intersection(
+      Set.from(
+        b,
+      ),
+    ),
+  );
 }
